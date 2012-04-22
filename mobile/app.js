c2349cd2e7d4e35fce5d2f176b0013ee8db59c5d
Christopher Sturgess <Chris@Cassandra.local>
2012-04-22 14:51:11 -0400

    Might have screwed it up

--- mobile/app.js
+++ mobile/app.js
@@ -12,14 +12,18 @@ Ext.application({
            
 			items: [
 			{
-				xtype: 'panel',
+				xtype: 'container',
+				layout: 'card',
+				autoEl: 'div',
 				dockedItems:[
     	        {
         	    	xtype: 'toolbar',
                 	title: 'About Me',
-	        	}],
-	        	html: '<h3>About Me</h3><p>My name is Christopher Sturgess. I am currently enrolled in Computer Science at the University of Waterloo, in my 2B term. I' + "'" + 've always had a passion for programming and software development, and I hope to continue working with new technology throughout my life.<br></p><p>You can contact me here:</p><table bordercolor="#000000"><tr><td>Email:</td><td/><td>christopher.sturgess@gmail.com</td></tr><tr><td>Github:</td><td/><td><a href="https://github.com/Chris-Sturgess">Chris-Sturgess</a></td></tr><tr><td>Stack Overflow:</td><td/><td><a href="http://stackoverflow.com/users/1144012/csturgess">CSturgess</a></td></tr></table><p>This website was developed using backbone.js and require.js.</p>'
-
+	        	},
+	        	{
+	        		xtype: 'panel',
+	        		html: 'My name is Christopher Sturgess. I am currently enrolled in Computer Science at the University of Waterloo, in my 2B term. I' + "'" + 've always had a passion for programming and software development, and I hope to continue working with new technology throughout my life.'
+				}]
             },
             {
             	html : 'Item 2',
