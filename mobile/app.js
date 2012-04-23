ebe516c666876c566d64f972a8c9cc6cec76f8b0
Christopher Sturgess <Chris@Cassandra.local>
2012-04-23 01:49:40 -0400

    Added Project and Work screens

--- mobile/app.js
+++ mobile/app.js
@@ -2,6 +2,48 @@ Ext.application({
 	name: 'Sencha',
     
     launch: function() {
+    
+    //Projects List
+    	Ext.define('Project', {
+			extend: 'Ext.data.Model',
+			config: {
+				fields: ['name', 'description']
+			}
+		});
+		
+		var projectStore = Ext.create('Ext.data.Store', {
+		   model: 'Project',
+		   sorters: 'name',
+		
+		   data: [
+			   { name: 'Crosby', description: 'A tablet application designed using HTML5, Zepto.js, and Ruby on Rails to measure the reaction time of medical patients. Built to tablet devices with PhoneGap.'  },
+			   { name: 'Foodlink', description: 'A mobile application for the Waterloo Region developed with HTML5, PhoneGap, and the Google Maps API. Designed to allow users to find local food stores.'  },
+			   { name: 'Wheatley', description: 'A Playbook application designed using HTML5 designed as a sign-in system for companies. Keeps track of current guests, with many customizable fields for data. Also creates QR Code badges for guests, allowing them to scan to sign in instead of re-entering all other information.'  },
+			   { name: 'FoodAtTheHub.com', description: 'An HTML website using Ruby on Rails to store information regarding food currently available at the Communitech Kitchen. Available at <a href="http://www.foodatthehub.com">www.foodatthehub.com</a>'  },
+			   { name: 'Yarn Shop', description: 'A native iOS application that allows customers to view selections and colours of yarn on their mobile devices.  <a href="http://itunes.apple.com/app/yarn-shop/id507312566?mt=8/">Currently available on the App Store.</a>'  },
+			   { name: 'www.christophersturgess.com', description: 'An online resume built using backbone.js and require.js for the main website, as well as Sencha Touch 2.0 for the mobile site. Available <a href="http://www.christophersturgess.com">here</a>. '  },
+		   ]
+		});
+    
+    //Work List
+    	Ext.define('Project', {
+			extend: 'Ext.data.Model',
+			config: {
+				fields: ['name', 'date', 'location', 'description']
+			}
+		});
+		
+		var workStore = Ext.create('Ext.data.Store', {
+		   model: 'Project',
+		   sorters: 'name',
+		
+		   data: [
+			   { name: 'Communitech Apps Factory', location: 'Kitchener, Ontario', date: 'January 2012 - April 2012',  description: '<ul><li>Used various web technologies to develop mobile applications on a wide range of devices</li><li>Worked with other students to create a total of 4 mobile applications over the course of the workterm</li></ul>'  },
+			   { name: 'Camilla Valley Farm Weavers Supply', location: 'Orangeville, Ontario', date: 'August 2011 - September 2011',  description: "<ul><li>Developed an iOS application to organize and display yarn colour swatch samples to be used by the online retailer's customers to help make purchasing decisions</li></ul>"  },
+			   { name: 'Tim Hortons', location: 'Orangeville, Ontario', date: 'April 2009 - September 2010',  description: "<ul><li>Performed various functions including: front counter cashier, food preparation, unloading supply trucks and clean up.</li><li>Gained experience in customer relations and working within a team environment</li></ul>"  }
+		   ]
+		});
+    
     	Ext.create('Ext.Carousel', {
         	fullscreen: true,
         	direction: 'horizontal',
@@ -25,10 +67,31 @@ Ext.application({
 				}]
             },
             {
-            	html : 'Item 2',
-                style: 'background-color: #5E99CC'
-        	}
-           	]
+				xtype: 'list',
+				itemTpl: '<div class="project"><strong>{name}</strong><br>{description}</div>',
+				store: projectStore,
+				width: '100%',
+				padding: '0',
+				items: [
+				{
+					xtype: 'toolbar',
+					title: 'Projects',
+					docked: 'top'
+				}]
+        	},
+        	{
+				xtype: 'list',
+				itemTpl: '<div class="work"><strong>{name}</strong><br>{location}, {date}<br>{description}</div>',
+				store: workStore,
+				width: '100%',
+				padding: '0',
+				items: [
+				{
+					xtype: 'toolbar',
+					title: 'Work History',
+					docked: 'top'
+				}]
+        	}]
         });
     }
 });
